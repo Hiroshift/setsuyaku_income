dc8808060b7d87e02c38ae29eb6c3d3905e88f66
Hiroshift <ooh-baby@buggies.co.jp>
2025-01-02 14:58:00 +0900

    Basic認証環境変数設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
   def hello
     render html: "Welcome to Rails!"
+  end
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER_SETSUYAKU"] && password == ENV["BASIC_AUTH_PASSWORD_SETSUYAKU"]
+    end
   end
 end
