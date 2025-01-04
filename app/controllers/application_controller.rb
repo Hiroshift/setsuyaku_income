e655a2451396ee59bf4c88cf63c7f89bededc185
Hiroshift <ooh-baby@buggies.co.jp>
2025-01-04 20:14:06 +0900

    ユーザー認証機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
-  def hello
-    render html: "Welcome to Rails!"
-  end
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
@@ -10,5 +8,9 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER_SETSUYAKU"] && password == ENV["BASIC_AUTH_PASSWORD_SETSUYAKU"]
     end
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :hourly_rate])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define the root path route ("/")
-  root "application#hello"
+  root 'home#index'
+  # post "new_income", to: "home#create"
 end
