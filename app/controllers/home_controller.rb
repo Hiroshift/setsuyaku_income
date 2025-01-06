c26e5610f87b73364435206ac4180f8da2f051f9
Hiroshift <ooh-baby@buggies.co.jp>
2025-01-07 00:03:32 +0900

    homeControllerのindexアクション実装とフラッシュメッセージの実装

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,17 +2,31 @@ class HomeController < ApplicationController
   before_action :authenticate_user! # ログインしているユーザーのみアクセス可能
 
   def index
-    @today_income = 0
-    @virtual_work_time = 0
-    @total_income = 0
+    today = Date.today
+
+    # 今日の収入
+    @today_income = current_user.recordings.where(recorded_date: today).sum(:amount)
+
+    # 累計収入
+    @total_income = current_user.recordings.sum(:amount)
+
+    # 仮想労働時間（hourly_rate が 0 の場合は 0 時間）
+    @virtual_work_time = if current_user.hourly_rate.positive?
+                           (@today_income.to_f / current_user.hourly_rate).round(2)
+                         else
+                           0
+                         end
   end
 
   def create
     amount = params[:amount].to_i
     if amount.positive?
       # 新しい記録をデータベースに保存
-      current_user.recordings.create(amount: amount, recorded_date: Date.today)
-      flash[:notice] = "節約金額が記録されました！"
+      if current_user.recordings.create(amount: amount, recorded_date: Date.today)
+        flash[:notice] = "節約金額が記録されました！"
+      else
+        flash[:alert] = "記録に失敗しました。もう一度お試しください。"
+      end
     else
       flash[:alert] = "節約金額を正しく入力してください。"
     end
