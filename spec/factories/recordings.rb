550245316e1233677679870ea0c055a03aad094e
Hiroshift <ooh-baby@buggies.co.jp>
2025-01-06 23:10:49 +0900

    FactoryBot作成とテストコード記述

--- spec/factories/recordings.rb
+++ spec/factories/recordings.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :recording do
-    amount { 1 }
-    recorded_date { "2025-01-06" }
-    user { nil }
+    amount { 1 } # 金額をデフォルトで1に設定
+    recorded_date { Date.today } # 今日の日付をデフォルトで設定
+    association :user # Userとの関連付けを明示
   end
 end
--- spec/models/recording_spec.rb
+++ spec/models/recording_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Recording, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @recording = FactoryBot.build(:recording)
+  end
+
+  describe 'Recordingの保存' do
+    context '保存できる場合' do
+      it 'amount, recorded_date, userが正しく設定されていれば保存できる' do
+        expect(@recording).to be_valid
+      end
+    end
+
+    context '保存できない場合' do
+      it 'amountが空の場合は保存できない' do
+        @recording.amount = nil
+        expect(@recording).not_to be_valid
+        expect(@recording.errors.full_messages).to include("Amount can't be blank")
+      end
+
+      it 'recorded_dateが空の場合は保存できない' do
+        @recording.recorded_date = nil
+        expect(@recording).not_to be_valid
+        expect(@recording.errors.full_messages).to include("Recorded date can't be blank")
+      end
+
+      it 'userが紐付いていない場合は保存できない' do
+        @recording.user = nil
+        expect(@recording).not_to be_valid
+        expect(@recording.errors.full_messages).to include("User must exist")
+      end
+    end
+  end
 end
